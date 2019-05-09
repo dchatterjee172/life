277e2c855cba7522652f9467151c5de100a2339c
Debajyoti Chatterjee <dchatterjee172@gmail.com>
2019-05-10 01:03:35 +0530

    use window

--- display/curse.py
+++ display/curse.py
@@ -1,5 +1,8 @@
 import curses
 
+scr = curses.initscr()
+curses.curs_set(0)
+
 
 class Display:
     def __init__(self, height, width, begin_x, begin_y):
@@ -8,13 +11,10 @@ class Display:
         self.begin_x = begin_x
         self.begin_y = begin_y
         self.height = height
-        self.scr = curses.initscr()
-        self.scr.clear()
-        self.scr.refresh()
-        curses.curs_set(0)
-        curses.endwin()
+        self.win = curses.newwin(height, width, begin_y, begin_x)
 
     def show(self, board):
+        self.win.clear()
         if len(board) > self.height:
             raise ValueError(
                 f"num of row in board {len(board)}, max height {self.height}"
@@ -25,6 +25,5 @@ class Display:
                     f"num of cell in {i} row is {len(row)}, max width {self.width}"
                 )
             for j, cell in enumerate(row):
-                self.scr.addstr(i + self.begin_x, j + self.begin_y, cell)
-        self.scr.refresh()
-        curses.endwin()
+                self.win.addch(j, i, cell)
+        self.win.refresh()
--- test.py
+++ test.py
@@ -1,7 +1,10 @@
 from stuff import Env
 from display import Display
+import curses
 
 env = Env()
 env.print()
-dis = Display(10, 10, 0, 0)
+dis = Display(12, 12, 1, 1)
 dis.show(env.board)
+curses.napms(1000)
+curses.endwin()
