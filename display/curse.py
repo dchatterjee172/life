4a7e383f0695c3547e5336a5539d962e82668beb
Debajyoti Chatterjee <dchatterjee172@gmail.com>
2019-05-09 10:05:16 +0530

    WIP curses

--- display/curse.py
+++ display/curse.py
@@ -2,7 +2,26 @@ import curses
 
 
 class Display:
-    def __init__(self, env, height, width, begin_x, begin_y):
-        win = curses.newwin(height, width, begin_y, begin_x)
+    def __init__(self, height, width, begin_x, begin_y):
+        self.height = height
+        self.width = width
+        self.begin_x = begin_x
+        self.begin_y = begin_y
+        self.height = height
+        self.scr = curses.initscr()
+        self.win = curses.newwin(height, width, begin_y, begin_x)
         curses.curs_set(0)
         pass
+
+    def show(self, board):
+        if len(board) > self.height:
+            raise ValueError(
+                f"num of row in board {len(board)}, max height {self.height}"
+            )
+        for i, row in enumerate(board):
+            if len(row) > self.width:
+                raise ValueError(
+                    f"num of cell in {i} row is {len(row)}, max width {self.width}"
+                )
+            for j, cell in enumerate(row):
+                self.scr.addstr(i, j, cell)
--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 from stuff import Env
+from display import Display
 
 env = Env()
 env.print()
+dis = Display(10, 10, 0, 0)
+dis.show(env.board)
