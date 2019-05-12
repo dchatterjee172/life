994e96aa3617c5a43913216cb1bc160d296d8469
Debajyoti Chatterjee <dchatterjee172@gmail.com>
2019-05-12 23:43:14 +0530

    restructure

--- stuff/env.py
+++ stuff/env.py
@@ -1,16 +1,21 @@
 from stuff.conf import max_x, max_y
 from itertools import product
 from random import choices
+from collections import defaultdict
 
 
 class Env:
-    def __init__(self):
+    def __init__(self, all_stuff, cum_weights):
+        if len(all_stuff) != len(cum_weights):
+            raise ValueError("all_stuff, cum_weights should have same length")
         self.max_x = max_x
         self.max_y = max_y
         self.time = -1
         self._board = [[" " for x in range(max_x)] for y in range(max_y)]
-        self.board_dict = {(x, y): [] for x in range(max_x) for y in range(max_y)}
+        self.board_dict = defaultdict(list)
         self.board_size = max_x * max_y
+        self.all_stuff = all_stuff
+        self._populate(cum_weights)
 
     @property
     def board(self):
@@ -22,18 +27,14 @@ class Env:
                 self._board = " "
         return self._board
 
-    def populate(self, all_stuff, cum_weights):
-        if len(all_stuff) != len(cum_weights):
-            raise ValueError("all_stuff, cum_weights should have same length")
+    def _populate(self, cum_weights):
         self.time += 1
         for (x, y), cls in zip(
             product(range(max_x), range(max_y)),
-            choices(all_stuff, cum_weights=cum_weights, k=self.board_size),
+            choices(self.all_stuff, cum_weights=cum_weights, k=self.board_size),
         ):
             obj = cls(x=x, y=y)
             self.board_dict[x, y].append(obj)
 
     def next_step(self):
-        if self.time < 0:
-            raise Exception("populate first")
         self.time += 1
--- test.py
+++ test.py
@@ -3,8 +3,7 @@ from display import Display
 import curses
 
 all_stuff = (Grass, Tiger, Rabbit)
-env = Env()
-env.populate(all_stuff=all_stuff, cum_weights=(0.5, 0.7, 1))
+env = Env(all_stuff=all_stuff, cum_weights=(0.5, 0.7, 1))
 dis = Display(12, 12, 1, 1)
 dis.show(env.board)
 curses.napms(1000)
